7630fbc84fd71251d3f79aa52e51e41eeb4bce6f
Jisan Zaman <jzaman@bstonetech.com>
2015-08-28 18:06:26 -0400

    added jquery mobile input range

--- MAS/js/app.js
+++ MAS/js/app.js
@@ -1,33 +1,76 @@
 angular.module('MAS', [
-  
-]).controller('SearchAndFilter', ['$scope', function($scope){
-	$scope.categories = [
-		{
-			name: 'Research',
-			active : true,
-			filterCategories : ['Style','Strategy', 'Fund', 'Account Min.', 'Asset Class', 'Status']
-		}, {
-			name: 'Requirements',
-			active : false,
-			filterCategories : ['Fun 1', "Fun 2"]
-		} , {
-			name: 'Tax',
-			active : false,
-			filterCategories : ['Tax 1' , 'Tax2']
-		}
-	];
 
-	$scope.selectCategory = function(category){
-		$scope.selectedcategory.active = false;
-		delete $scope.selectedcategory;
-		$scope.selectedcategory = category;
-		category.active = true;
-	};
+]).controller('SearchAndFilter', ['$scope',
+  function($scope) {
+    $scope.categories = [{
+      name: 'Research',
+      active: true,
+      filterCategories: [{
+        name: 'Style',
+        dataType: 'category'
+      }, {
+        name: 'Strategy',
+        dataType: 'category'
+      }, {
+        name: 'Fund',
+        dataType: 'category'
+      }, {
+        name: 'Account Min.',
+        dataType: 'number'
+      }, {
+        name: 'Asset Class',
+        dataType: 'category'
+      }, {
+        name: 'Status',
+        dataType: 'category'
+      }]
+    }, {
+      name: 'Requirements',
+      active: false,
+      filterCategories: [{
+        name: 'Requirements 1',
+        dataType: 'category'
+      }, {
+        name: "Requirements 2",
+        dataType: 'category'
+      }]
+    }, {
+      name: 'Tax',
+      active: false,
+      filterCategories: [{
+        name: 'Tax 1',
+        dataType: 'category'
+      }, {
+        name: 'Tax2',
+        dataType: 'category'
+      }]
+    }];
 
-	$scope.selectedcategory = $scope.categories[0];
-	$scope.selectedcategory.active = true;
-}]);
+    $scope.selectCategory = function(category) {
+      $scope.selectedcategory.active = false;
+      delete $scope.selectedcategory;
+      $scope.selectedcategory = category;
+      category.active = true;
+    };
 
-angular.element(document).ready(function(){
-	angular.bootstrap(document, ['MAS'])
+    $scope.selectedcategory = $scope.categories[0];
+    $scope.selectedcategory.active = true;
+  }
+]);
+
+angular.element(document).ready(function() {
+  angular.bootstrap(document, ['MAS']);
+  $(function() {
+    $("#slider-range").slider({
+      range: true,
+      min: 0,
+      max: 10000000,
+      values: [3000000, 5000000],
+      slide: function(event, ui) {
+        $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+      }
+    });
+    $("#amount").val("$" + $("#slider-range").slider("values", 0) +
+      " - $" + $("#slider-range").slider("values", 1));
+  });
 })
