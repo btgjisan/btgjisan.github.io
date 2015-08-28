3ce7f7decf7c51de10fabb6dfed073db3d77feac
Jisan Zaman <jzaman@bstonetech.com>
2015-08-28 14:53:21 -0400

    added MAS

---
+++ MAS/js/app.js
@@ -0,0 +1,33 @@
+angular.module('MAS', [
+  
+]).controller('SearchAndFilter', ['$scope', function($scope){
+	$scope.categories = [
+		{
+			name: 'Research',
+			active : true,
+			filterCategories : ['Strategy', 'Fund', 'Account Min.', 'Asset Class', 'Status']
+		}, {
+			name: 'Requirements',
+			active : false,
+			filterCategories : ['Fun 1', "Fun 2"]
+		} , {
+			name: 'Tax',
+			active : false,
+			filterCategories : ['Tax 1' , 'Tax2']
+		}
+	];
+
+	$scope.selectCategory = function(category){
+		$scope.selectedcategory.active = false;
+		delete $scope.selectedcategory;
+		$scope.selectedcategory = category;
+		category.active = true;
+	};
+
+	$scope.selectedcategory = $scope.categories[0];
+	$scope.selectedcategory.active = true;
+}]);
+
+angular.element(document).ready(function(){
+	angular.bootstrap(document, ['MAS'])
+})
